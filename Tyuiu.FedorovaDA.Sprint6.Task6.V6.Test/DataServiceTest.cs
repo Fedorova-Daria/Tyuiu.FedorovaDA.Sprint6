74ccce443066eec6e1e1c4a9c8a14138eea372ab
Дарья Федорова <Tyuiu.FedorovaDA@outlook.com>
2024-11-07 10:25:21 +0500

    Task6

--- Tyuiu.FedorovaDA.Sprint6.Task6.V6.Test/DataServiceTest.cs
+++ Tyuiu.FedorovaDA.Sprint6.Task6.V6.Test/DataServiceTest.cs
@@ -1,12 +1,10 @@
+namespace Tyuiu.FedorovaDA.Sprint6.Task6.V6.Test;
 
-namespace Tyuiu.FedorovaDA.Sprint6.Task6.V6.Test
+[TestClass]
+public class DataServiceTest
 {
-    [TestClass]
-    public class DataServiceTest
+    [TestMethod]
+    public void TestMethod1()
     {
-        [TestMethod]
-        public void TestMethod1()
-        {
-        }
     }
 }
